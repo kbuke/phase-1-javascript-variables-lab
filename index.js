3346088247ed9cb5a73e8edd31dfd694ef59b825
kabuke <kabuke13@gmail.com>
2023-07-04 15:56:04 +0100

    Required Changes

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+const companyName = "Scuber" //Set companyName to Scuber. Its value can not be changed.
+
+let mostProfitableNeighborhood = "Chelsea" //Use let so that we can change value
+
+let companyCeo = "Susan Smith"
